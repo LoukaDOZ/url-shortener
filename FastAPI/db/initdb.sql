7734ae8099bba87290c195b394706f5ab94a5763
doz <dozlouka@gmail.com>
2024-01-16 18:05:49 +0100

    rename init.sql to initdb.sql

---
+++ FastAPI/db/initdb.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE url_shortener;
+\c url_shortener;
+
+CREATE TABLE users(
+    username varchar(32) PRIMARY KEY NOT NULL,
+    password varchar(64) NOT NULL
+);
+
+CREATE TABLE url(
+    url_id varchar(8) PRIMARY KEY NOT NULL,
+    target_url varchar(512) NOT NULL,
+    expiration_date bigint NOT NULL,
+    username varchar(32) REFERENCES users(username)
+);
